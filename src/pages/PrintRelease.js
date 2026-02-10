86eb996203a3ddc87adbc851a778b1cfef7deaba
yash02604 <yashmhatre2004@gmail.com>
2026-02-10 23:48:38 +0530

    Version 43

--- src/pages/PrintRelease.js
+++ src/pages/PrintRelease.js
@@ -1174,7 +1174,6 @@ const PrintRelease = () => {
           if (isPdf || isImage) {
             // Convert to blob URL for proper handling
             const byteString = atob(dataUrl.split(',')[1]);
-            const mimeString = dataUrl.split(',')[0].split(':')[1].split(';')[0];
             const ab = new ArrayBuffer(byteString.length);
             const ia = new Uint8Array(ab);
             for (let i = 0; i < byteString.length; i++) {
